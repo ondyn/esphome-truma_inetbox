ed75db68bcef2736f2cbce127a96044c01a4aea7
Ondrej Hnyk <ondrej.hnyk@t-mobile.cz>
2025-07-08 15:58:07 +0200

    fix: u_int8_t --> uint8_t

--- components/truma_inetbox/climate/TrumaRoomClimate.cpp
+++ components/truma_inetbox/climate/TrumaRoomClimate.cpp
@@ -50,7 +50,7 @@ void TrumaRoomClimate::dump_config() { LOG_CLIMATE(TAG, "Truma Room Climate", th
 void TrumaRoomClimate::control(const climate::ClimateCall &call) {
   if (call.get_target_temperature().has_value() && !call.get_fan_mode().has_value()) {
     float temp = *call.get_target_temperature();
-    this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp));
+    this->parent_->get_heater()->action_heater_room(static_cast<uint8_t>(temp));
   }
 
   if (call.get_mode().has_value()) {
@@ -89,13 +89,13 @@ void TrumaRoomClimate::control(const climate::ClimateCall &call) {
     }
     switch (fan_mode) {
       case climate::CLIMATE_FAN_LOW:
-        this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp), HeatingMode::HEATING_MODE_ECO);
+        this->parent_->get_heater()->action_heater_room(static_cast<uint8_t>(temp), HeatingMode::HEATING_MODE_ECO);
         break;
       case climate::CLIMATE_FAN_MEDIUM:
-        this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp), HeatingMode::HEATING_MODE_HIGH);
+        this->parent_->get_heater()->action_heater_room(static_cast<uint8_t>(temp), HeatingMode::HEATING_MODE_HIGH);
         break;
       case climate::CLIMATE_FAN_HIGH:
-        this->parent_->get_heater()->action_heater_room(static_cast<u_int8_t>(temp), HeatingMode::HEATING_MODE_BOOST);
+        this->parent_->get_heater()->action_heater_room(static_cast<uint8_t>(temp), HeatingMode::HEATING_MODE_BOOST);
         break;
       default:
         this->parent_->get_heater()->action_heater_room(0);
